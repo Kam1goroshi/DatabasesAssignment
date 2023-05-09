4d3c76f4e746c609984ab3fb3e7b5811cf6045e5
gpappa01 <gpappa01@ucy.ac.cy>
2023-05-09 21:36:30 +0300

    Implement getCustomersByName
    Return an arraylist of customers from the db

--- src/main/java/db_agent/CustomersAgent.java
+++ src/main/java/db_agent/CustomersAgent.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class CustomersAgent {
 
@@ -47,5 +48,32 @@ public class CustomersAgent {
             e.printStackTrace();
         }
         return null;
+    }
+
+    /**
+     * Retrieves all the customers with provided name as a substring in their name and returns them as arraylist
+     * @param name civilian name
+     * @return Customer arraylist, size 0 on no customers, or null on fail connection
+     */
+    public static ArrayList<Customer> getCustomersByName(String name){
+        Connection conn;
+        Statement stmt;
+        ArrayList<Customer> list = null;
+        Customer customer = null;
+        try{
+        conn = DatabaseConnection.getConnection();
+        stmt = conn.createStatement();
+        ResultSet result = stmt.executeQuery("SELECT * FROM customers WHERE customers.name LIKE '%"+name+"%';");
+        list = new ArrayList<>();
+        while(result.next()){
+            System.out.println("hello???");
+            customer = new Customer(result);
+            list.add(customer);
+        }
+        conn.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return list;
     }
 }
--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -8,9 +8,9 @@ import db_agent.DatabaseConnection;
 
 public class App {
     public static void main(String[] args) {
-        Customer customer = new Customer("BJ339081", "Big Jacob", "+559965345223", "bigJacob@jacobs.com", Date.valueOf("1995-03-05"));
-        CustomersAgent.addCustomer(customer);
-        System.out.println(CustomersAgent.getCustomer("BJ339081"));
+        //Customer customer = new Customer("BJ539933", "Big Jacob", "+5596745223", "bigJacob@hotmail.com", Date.valueOf("1995-03-05"));
+        //CustomersAgent.addCustomer(customer);
+        System.out.println(CustomersAgent.getCustomersByName("Big Jacob"));
     }
 }
 
