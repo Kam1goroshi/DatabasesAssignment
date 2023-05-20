84455eed4cfc3d630aa31f567239c2cea3b21f41
gpappa01 <giorgos.pappas1@hotmail.com>
2023-05-20 13:13:06 +0300

    Comment out test prints from main

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -28,26 +28,26 @@ public class App extends Application {
     }
     
     public static void main(String[] args) {
-        // Customer customer = new Customer("BJ539933", "Big Jacob", "+5596745223",
-        // "bigJacob@hotmail.com", Date.valueOf("1995-03-05"));
-        CustomersAgent customersAgent = new CustomersAgent();
-        RoomsAgent roomsAgent = new RoomsAgent();
-        ReservationsAgent reservationsAgent = new ReservationsAgent();
-        // CustomersAgent.addCustomer(customer);
-        System.out.println(customersAgent.getCustomersByName("Big Jacob"));
-        System.out.println(customersAgent.getCustomer("BJ339081"));
-        System.out.println(roomsAgent.getRoomByID(3));
-        System.out.println(roomsAgent.getRoomByNumber("A233"));
-        System.out.println(roomsAgent.getAvailableRooms(Date.valueOf("2023-04-01"), Date.valueOf("2023-4-3")));
-        System.out.println(reservationsAgent.addReservation(
-                new Reservation(
-                        roomsAgent.getRoomByNumber("A233").getID(),
-                        "BJ339081",
-                        Date.valueOf("2023-12-05"),
-                        Date.valueOf("2023-12-07"),
-                        150.0,
-                        Reservation.Status.RESERVED,
-                        false)));
+        // // Customer customer = new Customer("BJ539933", "Big Jacob", "+5596745223",
+        // // "bigJacob@hotmail.com", Date.valueOf("1995-03-05"));
+        // CustomersAgent customersAgent = new CustomersAgent();
+        // RoomsAgent roomsAgent = new RoomsAgent();
+        // ReservationsAgent reservationsAgent = new ReservationsAgent();
+        // // CustomersAgent.addCustomer(customer);
+        // System.out.println(customersAgent.getCustomersByName("Big Jacob"));
+        // System.out.println(customersAgent.getCustomer("BJ339081"));
+        // System.out.println(roomsAgent.getRoomByID(3));
+        // System.out.println(roomsAgent.getRoomByNumber("A233"));
+        // System.out.println(roomsAgent.getAvailableRooms(Date.valueOf("2023-04-01"), Date.valueOf("2023-4-3")));
+        // System.out.println(reservationsAgent.addReservation(
+        //         new Reservation(
+        //                 roomsAgent.getRoomByNumber("A233").getID(),
+        //                 "BJ339081",
+        //                 Date.valueOf("2023-12-05"),
+        //                 Date.valueOf("2023-12-07"),
+        //                 150.0,
+        //                 Reservation.Status.RESERVED,
+        //                 false)));
         launch();
     }
 }
